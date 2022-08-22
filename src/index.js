c576ff81ee04bcafc0f9bc098481d6b4eaf46455
pippi1218 <ma7pippi@gmail.com>
2022-08-22 22:31:09 +0900

    マスをクリックしたときに、クリックしたマスに"X"と表示

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import "./index.css";
  * 正方形のマス目コンポーネント
  */
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => console.log("click")}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
       </button>
     );
   }
