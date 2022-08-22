7b6c2cb0130884a33090f51cc6e87c50ec130edd
pippi1218 <ma7pippi@gmail.com>
2022-08-22 23:11:12 +0900

    handleClickメソッドの追加

--- src/index.js
+++ src/index.js
@@ -26,6 +26,12 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();\
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
     return (
       <Square
